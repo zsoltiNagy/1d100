337bb7da8db1bfd8ba65248fb2de1d4135653626
zsoltiNagy <zsoltiusz.nagy@gmail.com>
2019-07-28 19:06:32 +0200

    Just the very base of it.

---
+++ 1d100/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import d100_json from './data/d100_test.json'
+
+function App() {
+  return (
+        <div className="App">
+            <Typography variant={"h4"}>1d100</Typography>
+
+        </div>
+  );
+}
+
+export default App;
